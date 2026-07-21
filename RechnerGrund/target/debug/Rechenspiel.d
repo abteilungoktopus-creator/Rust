a4236d55721c9b5f7f0c328718174a7fc2850629
abteilungoktopus-creator <abteilung.oktopus@gmail.com>
2026-07-21 18:35:05 +0200

    Add files via upload

---
+++ RechnerGrund/target/debug/Rechenspiel.d
@@ -0,0 +1,1 @@
+/home/jonas/Rust/RechnerGrund/target/debug/Rechenspiel: /home/jonas/Rust/RechnerGrund/src/main.rs
---
+++ RechnerGrund/target/debug/deps/Rechenspiel-586cc7e196ee2e1f.d
@@ -0,0 +1,5 @@
+/home/jonas/Rust/Rechner/target/debug/deps/Rechenspiel-586cc7e196ee2e1f.d: src/main.rs
+
+/home/jonas/Rust/Rechner/target/debug/deps/Rechenspiel-586cc7e196ee2e1f: src/main.rs
+
+src/main.rs:
